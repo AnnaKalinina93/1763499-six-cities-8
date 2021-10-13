5583da15f0e806b7507b0d589577dd573103ca70
Anna Kalinina <annakalinina13071993@gmail.com>
2021-10-13 22:33:28 +0300

    добавила маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,41 @@
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import Property from '../../pages/property/property';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PrivateRoute from '../../components/private-route/private-route';
 
 type AppProps = {
-  offers: number[]
-}
+  offers: number[];
+};
 
 function App({ offers }: AppProps): JSX.Element {
   return (
-    <Main offers={offers} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <Main offers={offers} />
+        </Route>
+        <Route exact path={AppRoute.Login}>
+          <Login />
+        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.Favorites}
+          render={() => <Favorites/>}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
+        <Route exact path={AppRoute.Room}>
+          <Property />
+        </Route>
+        <Route>
+          <NotFoundScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
